b23e2264bf156a7098526ef8147f1d09667248ff
Taumah <ttresgots@gmail.com>
2020-01-18 12:41:47 +0100

    last text remove

--- src/views/win_log_in.c
+++ src/views/win_log_in.c
@@ -1,13 +1,3 @@
-#define SERVER "localhost"
-#define USER "root"
-#define PASSWORD "root"
-#define DATABASE "project"
-#define PREPARE_CONNECTION(connector) connector = mysql_init(NULL);\
-                                      if (!mysql_real_connect(connector, SERVER, USER, PASSWORD, DATABASE, 0, NULL, 0)) {\
-                                          fprintf(stderr, "%s\n", mysql_error(connector));\
-                                          exit(1);\
-                                      }                                 
-
 #include <gtk/gtk.h>
 #include <mysql.h>
 #include <stdio.h>
