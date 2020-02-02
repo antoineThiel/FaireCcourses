97b765e6403ba78dad293faeeaa0051b8ce2e0a6
Taumah <ttresgots@gmail.com>
2020-02-02 18:31:16 +0100

    add make launch to make file

---
+++ headers/all.h
@@ -0,0 +1,4 @@
+#include "include.h"
+#include "gtk_fonctions.h"
+#include "sql_fonctions.h"
+#include "views.h"
---
+++ headers/include.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <mysql.h>
+#include <gtk/gtk.h> 
