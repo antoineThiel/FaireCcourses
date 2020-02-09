0d7b68fafa51c05776913df87988cc5889a5a4e0
antoineThiel <antoine.thiel@hotmail.fr>
2020-02-09 13:06:32 +0100

    sql

--- dump.sql
+++ dump.sql
@@ -1,176 +1,60 @@
--- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
---
--- Host: localhost    Database: project
--- ------------------------------------------------------
--- Server version	5.7.28-0ubuntu0.18.04.4
+create database project;
+create table customer(id int auto_increment primary key, username varchar(40), password varchar(40), total_order int, `status` bool);
+insert into customer values (NULL, 'toinou', 'toinou', 0, 1);
+insert into customer values (NULL, 'thomas', 'thomas', 0, 1);
+insert into customer values (NULL, 'client', 'client', 0, 0);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `category`
---
+create table store(id int auto_increment primary key, name varchar(50));
+insert into store values (NULL, 'Auchan');
+insert into store values (NULL, 'Lidl');
+insert into store values (NULL, 'Super U');
+insert into store values (NULL, 'Leclerc');
 
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+create table category(id int auto_increment primary key, name varchar(50), id_store int);
+insert into category values ( NULL, 'viande', 1);
+insert into category values ( NULL, 'fruits', 1);
+insert into category values ( NULL, 'légumes', 1);
+insert into category values ( NULL, 'gâteaux', 1);
+insert into category values ( NULL, 'bonbons', 1);
+insert into category values ( NULL, 'féculents', 1);
+insert into category values ( NULL, 'produits de toilettes', 1);
+insert into category values ( NULL, 'jardinage', 1);
+insert into category values ( NULL, 'habits hommes', 1);
+insert into category values ( NULL, 'habits femmes', 1);
+insert into category values ( NULL, 'habits enfants', 1);
+insert into category values ( NULL, 'congelés', 1);
 
---
--- Dumping data for table `category`
---
+create table product(id int auto_increment primary key, name varchar(50), category varchar(50), price double);
+insert into product values ( NULL, 'bavette', 'viande', 5);
+insert into product values ( NULL, 'pomme', 'fruits', 1);
+insert into product values ( NULL, 'carottes', 'légumes', 1);
+insert into product values ( NULL, 'prince', 'gâteaux', 2);
+insert into product values ( NULL, 'crocros', 'bonbons', 3);
+insert into product values ( NULL, 'coquillettes', 'féculents', 2);
+insert into product values ( NULL, 'brosse à dents', 'produits de toilettes', 3);
+insert into product values ( NULL, 'pelle', 'jardinage', 10);
+insert into product values ( NULL, 'slip', 'habits hommes', 5);
+insert into product values ( NULL, 'culottes', 'habits femmes', 5);
+insert into product values ( NULL, 'sweat', 'habits enfants', 10);
+insert into product values ( NULL, 'frites', 'congelés', 7);
 
-LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'Fruit'),(2,'Boisson'),(3,'Bonbon'),(4,'Viande');
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
-UNLOCK TABLES;
+create table product_store(id_store int, id_product int);
+insert into product_store values ( 1, 1);
+insert into product_store values ( 1, 2);
+insert into product_store values ( 1, 3);
+insert into product_store values ( 1, 4);
+insert into product_store values ( 1, 5);
+insert into product_store values ( 1, 6);
+insert into product_store values ( 1, 7);
+insert into product_store values ( 1, 8);
+insert into product_store values ( 1, 9);
+insert into product_store values ( 1, 10);
+insert into product_store values ( 1, 11);
+insert into product_store values ( 1, 12);
 
---
--- Table structure for table `customer`
---
+create table `order`(id int auto_increment primary key, id_customer int, price double, id_store int);
 
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `customer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+create table product_order(id_order int, id_product int, quantity int not null);
 
---
--- Dumping data for table `customer`
---
-
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,'antoine','toinou','toinou');
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `order`
---
-
-DROP TABLE IF EXISTS `order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_store` int(11) DEFAULT NULL,
-  `id_user` int(11) DEFAULT NULL,
-  `id_caca` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order`
---
-
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES (1,2,NULL,NULL),(2,2,NULL,NULL),(3,2,NULL,NULL),(4,2,NULL,NULL),(5,2,NULL,NULL),(6,2,NULL,NULL),(7,2,NULL,NULL),(8,2,NULL,NULL),(9,2,NULL,NULL),(10,2,NULL,NULL),(11,2,NULL,NULL),(12,3,NULL,NULL),(13,2,NULL,NULL),(14,2,NULL,NULL),(15,2,NULL,NULL),(16,2,NULL,NULL),(17,2,NULL,NULL),(18,2,NULL,NULL),(19,2,NULL,NULL),(20,2,NULL,NULL),(21,2,NULL,NULL),(22,2,NULL,NULL),(23,2,NULL,NULL),(24,2,NULL,NULL),(25,2,NULL,NULL),(26,2,NULL,NULL),(27,3,NULL,NULL),(28,2,NULL,NULL),(29,2,NULL,NULL),(30,4,NULL,NULL),(31,3,NULL,NULL);
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `product`
---
-
-DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(40) DEFAULT NULL,
-  `categorie` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `product`
---
-
-LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (43,'fanta','Boisson'),(44,'coca','Boisson'),(45,'schokobons','Bonbon'),(46,'eau','Boisson');
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `product_order`
---
-
-DROP TABLE IF EXISTS `product_order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `product_order` (
-  `id_order` int(11) DEFAULT NULL,
-  `id_product` int(11) DEFAULT NULL,
-  `quantity` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `product_order`
---
-
-LOCK TABLES `product_order` WRITE;
-/*!40000 ALTER TABLE `product_order` DISABLE KEYS */;
-INSERT INTO `product_order` VALUES (NULL,43,1),(NULL,14,1),(NULL,15,1),(NULL,17,1),(NULL,18,1),(NULL,19,1),(NULL,20,1),(NULL,22,1),(NULL,23,1),(NULL,24,3),(NULL,25,5),(26,44,4),(27,44,1),(31,44,1),(31,44,3);
-/*!40000 ALTER TABLE `product_order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `store`
---
-
-DROP TABLE IF EXISTS `store`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `store` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `store`
---
-
-LOCK TABLES `store` WRITE;
-/*!40000 ALTER TABLE `store` DISABLE KEYS */;
-INSERT INTO `store` VALUES (1,'super U'),(2,'carrefour'),(3,'auchan'),(4,'Lidl');
-/*!40000 ALTER TABLE `store` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-01-18 11:40:48
+create table shelf(id int auto_increment primary key, category varchar(50), id_store int, pos_x int, pos_y int);
